f91d7516f902d274fc347986d0b6bd4b9b1c0cbb
Sameer Sapkota <acidicsameer.namrmc008@gmail.com>
2025-10-20 07:28:56 +0545

    project added on project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import PageNotFound from "./Pages/PageNotFound";
 import AppLayout from "./components/Layout/AppLayout";
-import About from './Pages/About'
-import Projects from './Pages/Projects';
-import Skill from './Pages/Skill';
-import Contact from './Pages/Contact';
+import About from "./Pages/About";
+import Projects from "./Pages/Projects";
+import Skill from "./Pages/Skill";
+import Contact from "./Pages/Contact";
 const App = () => {
   return (
- <>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/skills" element={<Skill />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-  <BrowserRouter>
-    
-      <Routes>
-  
-<Route element={<AppLayout/>}>
-<Route    path='/' element={<Home/>}/>
-<Route    path='about' element={<About />}/>
-<Route    path='projects' element={<Projects/>}/>
-<Route    path='contact' element={<Contact/>}/>
-<Route    path='skills' element={<Skill/>}/>
-<Route  path="*"  element={<PageNotFound/>} /> 
-</Route>
-    </Routes>
-    </BrowserRouter>
-
-
- </>
-)}
-
-export default App
-App
+export default App;
+App;
